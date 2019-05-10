3c82167cd8b623a3ca76efa9930a0abb98260c3f
Bashir Saidi <wamulabash1@gmail.com>
2019-05-10 16:38:57 +0300

    initial commit

---
+++ db.py
@@ -0,0 +1,16 @@
+import psycopg2
+import psycopg2.extras
+from pprint import pprint
+import os
+
+class DatabaseConnection:
+
+    def __init__(self):
+        self.db_name = 'artadherence'
+
+    pprint(self.db_name)
+
+    try:
+        self.connection = psycopg2.connect()
+    except Exception as e:
+    # raise e
